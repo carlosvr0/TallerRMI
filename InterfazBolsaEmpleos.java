2d225e8fe48d9fb0942f0efe1b3affdd0351b5c7
AlejandraAbaunza <alejandraabaunza03@gmail.com>
2025-04-15 15:08:07 -0500

    Consulta por oferta agregada

--- InterfazBolsaEmpleos.java
+++ InterfazBolsaEmpleos.java
@@ -6,5 +6,6 @@ public interface InterfazBolsaEmpleos extends Remote{
     String registrarOfertasTrabajo(Empleo empleo) throws RemoteException;
     List<Empleo> consultarOfertasRegistradas() throws RemoteException;
     String inscribirseEnOferta(Inscripcion inscripcion) throws RemoteException;
+    List<Inscripcion> consultarInscripcionesPorOferta(String idOferta) throws RemoteException;
 
 }
